40171309ec10928e4b8cba3488ca8b42b2b411da
David Zheng <986945193@qq.com>
2016-09-15 23:03:53 +0800

    时间日期选择器 详解代码

---
+++ app/src/main/java/daviddatepickerdemo/qq986945193/com/daviddatepickerdemo/MainActivity.java
@@ -0,0 +1,74 @@
+package daviddatepickerdemo.qq986945193.com.daviddatepickerdemo;
+
+
+import java.util.Calendar;
+
+import android.app.Activity;
+import android.app.DatePickerDialog;
+import android.app.Dialog;
+import android.os.Bundle;
+import android.view.View;
+import android.view.View.OnClickListener;
+import android.widget.Button;
+import android.widget.DatePicker;
+import android.widget.TextView;
+
+/**
+ * 时间选择器
+ */
+public class MainActivity extends Activity {
+    int mYear, mMonth, mDay;
+    Button btn;
+    TextView dateDisplay;
+    final int DATE_DIALOG = 1;
+
+    @Override
+    public void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+        btn = (Button) findViewById(R.id.dateChoose);
+        dateDisplay = (TextView) findViewById(R.id.dateDisplay);
+
+        btn.setOnClickListener(new OnClickListener() {
+
+            @Override
+            public void onClick(View v) {
+                showDialog(DATE_DIALOG);
+            }
+        });
+
+        final Calendar ca = Calendar.getInstance();
+        mYear = ca.get(Calendar.YEAR);
+        mMonth = ca.get(Calendar.MONTH);
+        mDay = ca.get(Calendar.DAY_OF_MONTH);
+    }
+
+    @Override
+    protected Dialog onCreateDialog(int id) {
+        switch (id) {
+            case DATE_DIALOG:
+                return new DatePickerDialog(this, mdateListener, mYear, mMonth, mDay);
+        }
+        return null;
+    }
+
+    /**
+     * 设置日期 利用StringBuffer追加
+     */
+    public void display() {
+        dateDisplay.setText(new StringBuffer().append(mMonth + 1).append("-").append(mDay).append("-").append(mYear).append(" "));
+    }
+
+    private DatePickerDialog.OnDateSetListener mdateListener = new DatePickerDialog.OnDateSetListener() {
+
+        @Override
+        public void onDateSet(DatePicker view, int year, int monthOfYear,
+                              int dayOfMonth) {
+            mYear = year;
+            mMonth = monthOfYear;
+            mDay = dayOfMonth;
+            display();
+        }
+    };
+}
